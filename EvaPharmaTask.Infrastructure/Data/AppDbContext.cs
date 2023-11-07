067e7347c90eee9d9899c5b7e9772cbe74805b88
Abdelrahman Gamal <abdelrahmangksd@gmail.com>
2023-11-07 07:40:21 +0200

    SeedGradeData

--- EvaPharmaTask.Infrastructure/Data/AppDbContext.cs
+++ EvaPharmaTask.Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EvaPharmaTask.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace EvaPharmaTask.Infrastructure.Data
@@ -14,6 +17,24 @@ namespace EvaPharmaTask.Infrastructure.Data
             base.OnModelCreating(builder);
 
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            SeedGradeData(builder);
+        }
+
+        private static void SeedGradeData(ModelBuilder builder)
+        {
+            builder.Entity<Grade>().HasData(new Grade[]
+            {
+                new Grade { Id = 1, GradeLevel = "A+"},
+                new Grade { Id = 2, GradeLevel = "A"},
+                new Grade { Id = 3, GradeLevel = "B+"},
+                new Grade { Id = 4, GradeLevel = "B"},
+                new Grade { Id = 5, GradeLevel = "C+"},
+                new Grade { Id = 6, GradeLevel = "C"},
+                new Grade { Id = 7, GradeLevel = "D+"},
+                new Grade { Id = 8, GradeLevel = "D"},
+                new Grade { Id = 9, GradeLevel = "F"},
+            });
         }
     }
 }
---
+++ EvaPharmaTask.Infrastructure/Data/Migrations/20231107053943_SeendGradeData.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EvaPharmaTask.Infrastructure.Data.Migrations
+{
+    public partial class SeendGradeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Grade",
+                columns: new[] { "Id", "GradeLevel" },
+                values: new object[,]
+                {
+                    { 1, "A+" },
+                    { 2, "A" },
+                    { 3, "B+" },
+                    { 4, "B" },
+                    { 5, "C+" },
+                    { 6, "C" },
+                    { 7, "D+" },
+                    { 8, "D" },
+                    { 9, "F" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Grade",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Grade",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Grade",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Grade",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Grade",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Grade",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Grade",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Grade",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Grade",
+                keyColumn: "Id",
+                keyValue: 9);
+        }
+    }
+}
